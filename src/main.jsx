d6505a2aef1f092468e80bb9ebca631859b6deef
marwa elhaggar <marwaelhaggar741@gmail.com>
2025-12-15 22:27:00 +0200

    fix react router page not found on vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
+import { BrowserRouter } from 'react-router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  
+ 
 )
