e3ddd8d21b47ef05566f52402f25da8ea86e7d58
marwa elhaggar <marwaelhaggar741@gmail.com>
2025-12-15 22:13:17 +0200

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/portfolio";
 import Contact from "./pages/Contact";
